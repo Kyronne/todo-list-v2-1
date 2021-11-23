027c605cd8a184fff3f8e0c117f6c6097a044094
Ubuntu <kyron@development-server.tbbwz1zngbyulmlnaelg4xiihg.zx.internal.cloudapp.net>
2021-11-23 16:12:56 +0000

    Saving progress

--- frontend/application/routes.py
+++ frontend/application/routes.py
@@ -1,23 +1,24 @@
-from application import app, db
-from application.models import Tasks
+from application import app
 from application.forms import TaskForm
 from flask import render_template, request, redirect, url_for, jsonify
-from os import getenv
+import requests
 
 @app.route('/')
 @app.route('/home')
 def home():
-    all_tasks = Tasks.query.all()
-    return render_template('index.html', title="Home", all_tasks=all_tasks)
+    all_tasks = requests.get("http://todo-app-backend:5000/read/allTasks").json()
+    app.logger.info(f"Tasks: {all_tasks}")
+    return render_template('index.html', title="Home", all_tasks=all_tasks["tasks"])
 
 @app.route('/create/task', methods=['GET','POST'])
 def create_task():
     form = TaskForm()
 
     if request.method == "POST":
-        new_task = Tasks(description=form.description.data)
-        db.session.add(new_task)
-        db.session.commit()
+        response = requests.post(
+            "http://todo-app-backend:5000/create/task", 
+            json={"description": form.description.data}
+        )
         return redirect(url_for('home'))
 
     return render_template("create_task.html", title="Add a new Task", form=form)
